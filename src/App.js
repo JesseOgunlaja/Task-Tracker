eb8dc352ba99c060116dc58f83687abed632b642
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-04-19 14:45:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import bcrypt from "bcryptjs";
 const jsonServer = "https://json-server-4313.vercel.app/api";
 console.log(process.env)
-const API_KEY = "a;LN8*f](uUUuW?,}vG3YVnjhEn*?zC=XU9pnU.k"
+const API_KEY = process.env.API_KEY
 
 function App() {
   const newTaskTitle = useRef();
