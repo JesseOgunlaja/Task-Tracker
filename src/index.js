3b97cf9c3d77f5870da83f8e04dcf58c80794aa5
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-04-28 22:16:43 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-console.log(process.env.APIKEY)
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
