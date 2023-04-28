8eb2c780878206bd24374ee8e2b579ea05635d1d
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-04-28 21:22:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import bcrypt from "bcryptjs";
 const jsonServer = "https://json-server-4313.vercel.app/api";
-console.log(process.env.API_KEY)
-const API_KEY = "a;LN8*f](uUUuW?,}vG3YVnjhEn*?zC=XU9pnU.k"
 
 function App() {
-  const apiKey = process.env.API_KEY;
-  console.log(apiKey)
+  const API_KEY = process.env.API_KEY;
+  console.log(API_KEY)
   const newTaskTitle = useRef();
   const newTaskDate = useRef();
   const newTaskReminder = useRef();
