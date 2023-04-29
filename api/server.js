745905d00b3014db7a5f507728a8e35feaad618d
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-04-29 15:24:03 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -117,10 +117,4 @@ async function getUser(req, res, next) {
 
 app.listen(port, () => {
   console.log(`Server started`)
-})
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://task-tracker-4313.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-api-key");
-  next();
 });
