e54485f98ce90f2794f7745e75119e2094633865
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-04-29 21:27:32 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 // Middleware function to verify API key
 function apiKeyVerification(req, res, next) {
   const apiKey = req.headers['x-api-key'];
-  if (!apiKey || apiKey !== process.env.REACT_APP_MY_API_KEY) {
+  if (!apiKey || apiKey !== "hi") {
     return res.status(401).send("Unathourised");
   }
   next();
