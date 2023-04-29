0072ec152454274b8ac74f58b84ea72c1c0af675
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-04-29 15:04:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const jsonServer = "https://json-server-4313.vercel.app/api";
 
 function App() {
   const API_KEY = process.env.REACT_APP_MY_API_KEY;
-  console.log(API_KEY)
   const newTaskTitle = useRef();
   const newTaskDate = useRef();
   const newTaskReminder = useRef();
