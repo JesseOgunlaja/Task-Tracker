09f0c99333b79c686ba6c429dee27877d3759672
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-04-30 15:18:03 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -43,7 +43,7 @@ function apiKeyVerification(req, res, next) {
 app.get('/api/users', apiKeyVerification, async (req, res) => {
   try {
     const users = await User.find();
-    res.status(200).json(users).send(users);
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
