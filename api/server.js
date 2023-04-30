02acbdd175d6a8f8187e6ec192b3ff40bc86648f
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-04-30 16:19:53 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -96,7 +96,7 @@ app.patch('/api/users/:id', apiKeyVerification, getUser, async (req, res) => {
 // Delete a user
 app.delete('/api/users/:id', apiKeyVerification, getUser, async (req, res) => {
   try {
-    await res.user.remove();
+    await res.user.delete()
     res.json({ message: 'User deleted' });
   } catch (error) {
     res.status(500).json({ message: error.message });
