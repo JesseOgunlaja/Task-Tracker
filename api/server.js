0939462f55c788dd1dff9409daca9b50ca807a92
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-05-06 21:50:59 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -2,47 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-import CryptoJS from "crypto-js";
-const https = require("https");
+const CryptoJS = require("crypto-js");
+const { greenlock } = require("greenlock-express")
 
 const API_KEY = process.env.API_KEY;
 
-const host = "0.0.0.0";
-const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 80;
 
-const options = {
-  cert: process.env.APP_CERT,
-  ca: process.env.APP_BUNDLE,
-  key: process.env.APP_KEY,
+const lexConfig = {
+  server: "https://acme-v02.api.letsencrypt.org/directory",
+  email: "jesseogunlaja@gmail.com", // replace with your email
+  agreeTos: true,
+  approveDomains: ["tasktracker4313.online"], // replace with your domain
+  app: app,
 };
 
-const app = express();
+greenlock.init(lexConfig).ready()
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000,
-  socketTimeoutMS: 45000,
-});
-
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  tasks: [
-    {
-      task: { type: String },
-      date: { type: String },
-      reminder: { type: Boolean },
-    },
-  ],
-});
-
-const User = mongoose.model("User", UserSchema);
-
-app.use(bodyParser.json());
-
-// Middleware function to verify API key
 function apiKeyVerification(req, res, next) {
   const apiKey = req.headers["x-api-key"];
   const ENCRYPTION_KEY = process.env.REACT_APP_ENCRYPTION_KEY;
@@ -50,13 +27,36 @@ function apiKeyVerification(req, res, next) {
   const stringKey = CryptoJS.enc.Base64.stringify(parsedKey);
   const decryptedKey = CryptoJS.AES.decrypt(apiKey, stringKey).toString(
     CryptoJS.enc.Utf8
-    );
+  );
   if (!apiKey || decryptedKey !== API_KEY) {
-    return res.status(403).send("Unathourized");
+    return res.status(403).send("Unauthorized");
   }
   next();
 }
 
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+  });
+
+  const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    tasks: [
+      {
+        task: { type: String },
+        date: { type: String },
+        reminder: { type: Boolean },
+      },
+    ],
+  });
+
+  const User = mongoose.model("User", UserSchema);
+
+  app.use(bodyParser.json())
 // Get all users
 app.get("/api/users", apiKeyVerification, async (req, res) => {
   try {
@@ -137,10 +137,6 @@ async function getUser(req, res, next) {
   res.user = user;
   next();
 }
-
-const httpsServer = https.createServer(options, app);
-httpsServer.listen(port, host, () => {
-  console.log(`Server has started on port ${port}`);
-});
-
-module.exports = httpsServer;
+app.listen(port, () => {
+  console.log(`Server started`);
+})
