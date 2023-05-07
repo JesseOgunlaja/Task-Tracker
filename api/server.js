0350e155a5cb035c8eb453470e8c7b1d6a94e863
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-05-07 19:39:33 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -25,16 +25,16 @@ function apiKeyVerification(req, res, next) {
 }
 
 const proxyOptions = {
-  target: 'https://tasktracker4313.online/api/Users', // The URL of the server you want to proxy
+  target: 'https://tasktracker4313.online', // The URL of the server you want to proxy
   changeOrigin: true, // Needed for virtual hosted sites
   onProxyReq: (proxyReq, req, res) => {
     proxyReq.setHeader('x-api-key', API_KEY);
   }
 };
 
-const proxy = createProxyMiddleware('/api', proxyOptions);
+const proxy = createProxyMiddleware(proxyOptions);
 app.use(apiKeyVerification)
-app.use(proxy);
+app.use("/api",proxy);
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
