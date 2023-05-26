dfbf967b4aa22b8e96e0fce149ad5441d852ba34
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-05-26 21:11:05 +0100

    first commit

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -332,6 +332,9 @@ module.exports = function (webpackEnv) {
         // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
         // please link the files into your node_modules/ and let module-resolution kick in.
         // Make sure your source files are compiled, as they will not be processed in any way.
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+        }),
         new ModuleScopePlugin(paths.appSrc, [
           paths.appPackageJson,
           reactRefreshRuntimeEntry,
