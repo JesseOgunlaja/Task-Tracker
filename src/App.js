facb8c1e7f99eb3697a3d54a2c1b8aaeaa8da0d9
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-05-26 22:15:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CryptoJS from "crypto-js";
 const api = window.location.href + "api";
 
 function App() {
-  document.cookie.API_KEY = `APIKEY=${'hi'}; path=/`
+  document.cookie = `APIKEY=${'hi'}; path=/`
   const ADMIN_PASSWORD = process.env.REACT_APP_ADMIN_PASSWORD;
   const ENCRYPTION_KEY = process.env.REACT_APP_ENCRYPTION_KEY;
   const ENCRYPTION_SESSION_1 = process.env.REACT_APP_ENCRYPTION_SESSION_1;
