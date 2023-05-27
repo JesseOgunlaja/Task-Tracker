d442600a71fc327b6d0b14e50f759d8920e94baa
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-05-27 12:15:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -186,7 +186,10 @@ function App() {
   async function fetchPeople() {
     const SECRET_KEY = ENCRYPTION_KEY
     const payload = process.env.REACT_APP_API_KEY
-    const token = jwt.jws.JWS.sign({ apiKey: payload }, SECRET_KEY);
+    const header = { alg: "HS256", typ: "JWT" };
+    const sHeader = JSON.stringify(header);
+    const sPayload = JSON.stringify(payload);
+    const token = jwt.jws.JWS.sign('HS256', sHeader, sPayload, SECRET_KEY)
     document.cookie = `token=${token};path=/`
     const res = await fetch(`/api/Users`, {
       method: "GET",
