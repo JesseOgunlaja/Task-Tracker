95a666a84cf42d565443346d62c8530a78a40986
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-05-27 12:47:19 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -54,11 +54,11 @@ const authenticateJWT = (req, res, next) => {
     if (decoded.apiKey === API_KEY) {
       next();
     } else {
-      return res.status(401).json({ message: 'Invalid token.' });
+      return res.status(401).json({ message: decoded.apiKey });
     }
   } catch (error) {
     // Invalid token
-    return res.status(401).json({ message: 'Invalid token.' });
+    return res.status(401).json({ message: decoded.apiKey });
   }
 };
 
