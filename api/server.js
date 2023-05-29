1edb079e20d7544deda6033dbbef9ac5d71b3970
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-05-29 18:51:54 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -115,7 +115,7 @@ function encryptString(nameGiven) {
   return encrypted2;
 }
 
-app.post("/api/email/:id", async (req,res) => {
+app.post("/api/users/email/:id", async (req,res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
