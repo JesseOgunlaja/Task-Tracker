ece81ef422b61331ee88e1e9cd6f931e0b7158d9
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-06-01 20:46:41 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -255,7 +255,7 @@ app.patch("/api/users/user/resetPassword", authenticateJWTGlobal, async (req,res
 
 // Update a user
 app.patch("/api/users/user",authenticateJWTUser, async (req, res) => {
-  cache.clear(`/api/users/checkJWT/${req.cookies.authToken}`);
+  apicache.clear(`/api/users/checkJWT/${req.cookies.authToken}`);
   const user = await User.findOne({ name: req.body.username });
   if (req.body.name != null) {
     user.name = req.body.name;
