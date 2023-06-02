84edf7a8a27ecb1e747fd3663e9cc4f74ab45346
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-06-02 21:09:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -765,8 +765,8 @@ function App() {
     }
   }
 
-  async function deleteCookie() {
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+  async function deleteCookie(cookie) {
+    document.cookie = `${cookie}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
   }
 
   return (
